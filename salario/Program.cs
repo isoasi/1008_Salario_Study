46c5b386d3d7a1ddec6e6956761c190f43e2c29f
Paola <Paola@DESKTOP-L53JRFL>
2024-05-02 22:27:24 -0300

    Adicionar arquivos de projeto.

---
+++ salario/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int NUMBER, horaTrabalhada;
+
+        double valorHora, totalSALARY;
+
+        NUMBER = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        horaTrabalhada = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+        totalSALARY = valorHora * horaTrabalhada;
+
+        Console.WriteLine("NUMBER = " + NUMBER);
+
+        Console.WriteLine("SALARY = U$ " + totalSALARY.ToString("F2", CultureInfo.InvariantCulture));
+
+        Console.Read();
+
+    }
+
+}
